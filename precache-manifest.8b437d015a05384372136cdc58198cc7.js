c5e4c0fd5aa71ac8c7a8477d461ae9e77f7d23ff
Maria Bolanos <maria.bolanos@itexico.com>
2020-05-18 14:17:59 -0500

    Updates

---
+++ precache-manifest.8b437d015a05384372136cdc58198cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "799267255da3f4653804bf0aa8cef1b2",
+    "url": "/ayurveda-test/index.html"
+  },
+  {
+    "revision": "8b3233beae5f7aec5452",
+    "url": "/ayurveda-test/static/css/main.f380c83e.chunk.css"
+  },
+  {
+    "revision": "9c82ca7b16f353967e99",
+    "url": "/ayurveda-test/static/js/2.5e05137c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ayurveda-test/static/js/2.5e05137c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b3233beae5f7aec5452",
+    "url": "/ayurveda-test/static/js/main.1d3aceaa.chunk.js"
+  },
+  {
+    "revision": "15354b1562edda6c4b93",
+    "url": "/ayurveda-test/static/js/runtime-main.3d898ce4.js"
+  }
+]);
